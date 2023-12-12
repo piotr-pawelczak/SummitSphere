3380b4d0dfb17ad9c7d05684c05bc6f2049b89da
piotr-pawelczak <pecio@student.agh.edu.pl>
2023-12-12 20:22:13 +0100

    SUM-14 Add navbar

--- summit_sphere/frontend/src/App.js
+++ summit_sphere/frontend/src/App.js
@@ -1,10 +1,20 @@
-import Navbar from "./components/Navbar";
+import DrawerNavbar from "./components/DrawerNavbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "@mui/material";
+import SummitList from "./pages/SummitList";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <DrawerNavbar />
+      <Container maxWidth="lg" sx={{ paddingTop: 10}}>
+        <Routes>
+          <Route path="/" element={<SummitList />} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
