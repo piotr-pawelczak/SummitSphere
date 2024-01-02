db4000aa2e5ca30f7a2971597c78b827e0cca309
piotr-pawelczak <pecio@student.agh.edu.pl>
2024-01-02 22:15:26 +0100

    SUM-12 Add token auth

--- summit_sphere/settings/base.py
+++ summit_sphere/settings/base.py
@@ -22,6 +22,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
     "django_extensions",
 ]
 
@@ -122,6 +123,11 @@ SHELL_PLUS = "bpython"
 # ---------- REST FRAMEWORK ----------------
 
 REST_FRAMEWORK = {
+    # PAGINATION
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
+    # AUTHENTICATION
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
--- summit_sphere/users/models.py
+++ summit_sphere/users/models.py
@@ -1,12 +1,23 @@
 from typing import Any
 
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
+from rest_framework.authtoken.models import Token
 
 from summit_sphere.commons.models import TimeStampedModel
 
 from .managers import UserManager
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """Create auth token for created user."""
+    if created:
+        Token.objects.create(user=instance)
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
